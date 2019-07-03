4f07d13665910683b6f553cb9d8c089483b3c80b
stas <sipnetic@gmail.com>
2019-07-03 11:58:47 +0300

    Fix scan_image

--- examples/scan_image.c
+++ examples/scan_image.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
 #include <png.h>
 #include <zbar.h>
 
@@ -17,78 +20,120 @@
 # define png_set_expand_gray_1_2_4_to_8 png_set_gray_1_2_4_to_8
 #endif
 
-zbar_image_scanner_t *scanner = NULL;
-
-/* to complete a runnable example, this abbreviated implementation of
- * get_data() will use libpng to read an image file. refer to libpng
- * documentation for details
- */
-static void get_data (const char *name,
-                      int *width, int *height,
-                      void **raw)
+static int get_data(const char *name, int *width, int *height, void **raw)
 {
-    FILE *file = fopen(name, "rb");
-    if(!file) exit(2);
-    png_structp png =
-        png_create_read_struct(PNG_LIBPNG_VER_STRING,
-                               NULL, NULL, NULL);
-    if(!png) exit(3);
-    if(setjmp(png_jmpbuf(png))) exit(4);
-    png_infop info = png_create_info_struct(png);
-    if(!info) exit(5);
+    FILE *file;
+    png_structp png;
+    png_infop info;
+    int color, bits, row_bytes;
+    png_byte **rows = NULL;
+    int i;
+    int result = 0;
+    
+    *raw = NULL;
+
+    file = fopen(name, "rb");
+    if (!file)
+    {
+        fprintf(stderr, "Can't open %s: %s\n", name, strerror(errno));
+        goto cleanup;
+    }
+    png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png)
+    {
+        fprintf(stderr, "Can't read %s: not a valid PNG\n", name);
+        goto cleanup;
+    }
+    if (setjmp(png_jmpbuf(png)))
+    {
+        goto cleanup;
+    }
+    info = png_create_info_struct(png);
     png_init_io(png, file);
     png_read_info(png, info);
     /* configure for 8bpp grayscale input */
-    int color = png_get_color_type(png, info);
-    int bits = png_get_bit_depth(png, info);
-    if(color & PNG_COLOR_TYPE_PALETTE)
+    color = png_get_color_type(png, info);
+    bits = png_get_bit_depth(png, info);
+    if (color == PNG_COLOR_TYPE_PALETTE)
+    {
         png_set_palette_to_rgb(png);
-    if(color == PNG_COLOR_TYPE_GRAY && bits < 8)
+        //png_read_update_info(png, info);
+        //color = png_get_color_type(png, info);
+        //bits = png_get_bit_depth(png, info);
+        color |= PNG_COLOR_MASK_ALPHA;
+    } else
+    if (color == PNG_COLOR_TYPE_GRAY && bits < 8)
         png_set_expand_gray_1_2_4_to_8(png);
-    if(bits == 16)
+    if (bits == 16)
         png_set_strip_16(png);
-    if(color & PNG_COLOR_MASK_ALPHA)
-        png_set_strip_alpha(png);
-    if(color & PNG_COLOR_MASK_COLOR)
+    if (color & PNG_COLOR_MASK_COLOR)
         png_set_rgb_to_gray_fixed(png, 1, -1, -1);
-    /* allocate image */
+    if (color & PNG_COLOR_MASK_ALPHA)
+        png_set_strip_alpha(png);
+    png_read_update_info(png, info);
     *width = png_get_image_width(png, info);
     *height = png_get_image_height(png, info);
+    row_bytes = png_get_rowbytes(png, info);
+    if (row_bytes != *width)
+    {
+        fprintf(stderr, "Can't convert %s to grayscale: something wrong with libPNG\n", name);
+        goto cleanup;
+    }
+    /* allocate image */
     *raw = malloc(*width * *height);
-    png_bytep rows[*height];
-    int i;
-    for(i = 0; i < *height; i++)
-        rows[i] = *raw + (*width * i);
+    rows = malloc(*height * sizeof(png_byte*));
+    for (i = 0; i < *height; i++)
+        rows[i] = (png_byte *) *raw + (row_bytes * i);
     png_read_image(png, rows);
+    result = 1;
+    
+    cleanup:
+    png_destroy_read_struct(&png, NULL, NULL);
+    if (!result)
+    {
+        free(*raw);
+        *raw = NULL;
+    }
+    free(rows);
+    fclose(file);
+    return result;
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    if(argc < 2) return(1);
+    zbar_image_scanner_t *scanner;
+    zbar_image_t *image;
+    const zbar_symbol_t *symbol;
+    int width, height, n;
+    void *raw;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <PNG_File>\n", argv[0]);
+        return 1;
+    }
 
     /* create a reader */
     scanner = zbar_image_scanner_create();
 
     /* configure the reader */
-    zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_ENABLE, 1);
+    //zbar_image_scanner_set_config(scanner, 0, ZBAR_CFG_ENABLE, 1);
 
     /* obtain image data */
-    int width = 0, height = 0;
-    void *raw = NULL;
-    get_data(argv[1], &width, &height, &raw);
+    if (!get_data(argv[1], &width, &height, &raw)) return 2;
 
     /* wrap image data */
-    zbar_image_t *image = zbar_image_create();
+    image = zbar_image_create();
     zbar_image_set_format(image, zbar_fourcc('Y','8','0','0'));
     zbar_image_set_size(image, width, height);
     zbar_image_set_data(image, raw, width * height, zbar_image_free_data);
 
     /* scan the image for barcodes */
-    int n = zbar_scan_image(scanner, image);
+    n = zbar_scan_image(scanner, image);
 
     /* extract results */
-    const zbar_symbol_t *symbol = zbar_image_first_symbol(image);
-    for(; symbol; symbol = zbar_symbol_next(symbol)) {
+    for (symbol = zbar_image_first_symbol(image); symbol; symbol = zbar_symbol_next(symbol))
+    {
         /* do something useful with results */
         zbar_symbol_type_t typ = zbar_symbol_get_type(symbol);
         const char *data = zbar_symbol_get_data(symbol);
@@ -100,5 +145,5 @@ int main (int argc, char **argv)
     zbar_image_destroy(image);
     zbar_image_scanner_destroy(scanner);
 
-    return(0);
+    return 0;
 }
